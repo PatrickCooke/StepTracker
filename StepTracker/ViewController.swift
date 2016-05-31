11c6cee70e9cfaa1036df434301a2cc367507e78
PatrickCooke <cookepa1@gmail.com>
2016-05-31 16:54:33 -0400

    chart is up and techically it works

--- StepTracker/ViewController.swift
+++ StepTracker/ViewController.swift
@@ -11,14 +11,60 @@ import HealthKit
 import PNChart
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PNChartDelegate {
 
     var healthStore = HKHealthStore()
-    @IBOutlet weak var stepsLabel :UILabel!
     var dailyStepsArray = [String:Double]()
     var weeklyStepsArray = [String:Double]()
     var monthlyStepsArray = [String:Double]()
-    var dailyStepInt: Int!
+    var orderedArray = [(String,Double)]()
+    
+    var dailyStepInt                        :Int!
+    @IBOutlet weak var graphTypeSegControl  :UISegmentedControl!
+    
+    
+    
+    //MARK: - Graphing Function
+    
+    func displayBarChart() {
+        let myBarChart = PNBarChart(frame: CGRectMake(10,50,350,550))
+        var tempXArray = [String]()
+        var tempYArray = [Double]()
+        for (key, value) in orderedArray {
+            tempXArray.append(key)
+            tempYArray.append(value)
+        }
+        myBarChart.xLabels = tempXArray
+        myBarChart.yValues = tempYArray
+        
+        switch graphTypeSegControl.selectedSegmentIndex {
+        case 0:
+            myBarChart.barWidth = 40.0
+        case 1:
+            myBarChart.barWidth = 20.0
+        case 2:
+            myBarChart.barWidth = 40.0
+        default:
+            print("nothing)")
+        }
+        myBarChart.barBackgroundColor = UIColor.clearColor()
+        myBarChart.barRadius = 3.0
+        myBarChart.isShowNumbers = true
+        myBarChart.chartMarginTop = 10
+        myBarChart.chartMarginBottom = 35
+        myBarChart.showChartBorder = true
+        myBarChart.strokeColor = UIColor.greenColor()
+        myBarChart.isGradientShow = true
+        
+        myBarChart.strokeChart()
+        myBarChart.delegate = self
+        self.view.addSubview(myBarChart)
+    }
+    
+    func userClickedOnBarAtIndex(barIndex: Int) {
+        print("bar \(barIndex)")
+    }
+
     
     //MARK: - Pull Data Method
     
@@ -91,60 +137,70 @@ class ViewController: UIViewController {
     
     //MARK: - HealthKit Methods
     
-    func getUserData() {
+    @IBAction func getUserData(segControl: UISegmentedControl) {
         print("getting data")
-        updateUserDailyStepsLabel(0)
-        updateUserDailyStepsLabel(1)
-        updateUserDailyStepsLabel(2)
-        updateUserDailyStepsLabel(3)
-        updateUserDailyStepsLabel(4)
-        updateUserDailyStepsLabel(5)
-        updateUserDailyStepsLabel(6)
-        updateUserWeeklyStepsLabel(0)
-        updateUserWeeklyStepsLabel(1)
-        updateUserWeeklyStepsLabel(2)
-        updateUserWeeklyStepsLabel(3)
-        updateUserWeeklyStepsLabel(4)
-        updateUserWeeklyStepsLabel(5)
-        updateUserWeeklyStepsLabel(6)
-        updateUserWeeklyStepsLabel(7)
-        updateUserWeeklyStepsLabel(8)
-        updateUserWeeklyStepsLabel(9)
-        updateUserWeeklyStepsLabel(10)
-        updateUserWeeklyStepsLabel(11)
-        updateUserMonthlyStepsLabel(0)
-        updateUserMonthlyStepsLabel(1)
-        updateUserMonthlyStepsLabel(2)
-        updateUserMonthlyStepsLabel(3)
-        updateUserMonthlyStepsLabel(4)
-        updateUserMonthlyStepsLabel(5)
+        if segControl.selectedSegmentIndex == 0 {
+            updateUserDailyStepsLabel(0)
+            updateUserDailyStepsLabel(1)
+            updateUserDailyStepsLabel(2)
+            updateUserDailyStepsLabel(3)
+            updateUserDailyStepsLabel(4)
+            updateUserDailyStepsLabel(5)
+            updateUserDailyStepsLabel(6)
+        } else if segControl.selectedSegmentIndex == 1 {
+            updateUserWeeklyStepsLabel(0)
+            updateUserWeeklyStepsLabel(1)
+            updateUserWeeklyStepsLabel(2)
+            updateUserWeeklyStepsLabel(3)
+            updateUserWeeklyStepsLabel(4)
+            updateUserWeeklyStepsLabel(5)
+            updateUserWeeklyStepsLabel(6)
+            updateUserWeeklyStepsLabel(7)
+            updateUserWeeklyStepsLabel(8)
+            updateUserWeeklyStepsLabel(9)
+            updateUserWeeklyStepsLabel(10)
+            updateUserWeeklyStepsLabel(11)
+        } else if segControl.selectedSegmentIndex == 2 {
+            updateUserMonthlyStepsLabel(0)
+            updateUserMonthlyStepsLabel(1)
+            updateUserMonthlyStepsLabel(2)
+            updateUserMonthlyStepsLabel(3)
+            updateUserMonthlyStepsLabel(4)
+            updateUserMonthlyStepsLabel(5)
+        }
     }
     
     func printDailyInfo() {
         if dailyStepsArray.count == 7 {
             print("Daily Data In")
-            for (key, value) in dailyStepsArray {
+            orderedArray = dailyStepsArray.sort {$0.0 < $1.0}
+            for (key, value) in orderedArray {
                 print("day \(key): \(value)")
             }
+            displayBarChart()
         }
     }
     
     func printWeeklyInfo() {
         if weeklyStepsArray.count == 12 {
             print("Weekly Data In")
-            for (key, value) in weeklyStepsArray {
+            orderedArray = weeklyStepsArray.sort {Int($0.0) < Int($1.0)}
+            for (key, value) in orderedArray {
                 print("week \(key): \(value)")
             }
+            displayBarChart()
         }
     }
     
     func printMonthlyInfo() {
         if monthlyStepsArray.count == 6 {
             print("Monthly Data In")
-            for (key, value) in monthlyStepsArray {
+            orderedArray = monthlyStepsArray.sort {$0.0 < $1.0}
+            for (key, value) in orderedArray {
                 print("Month \(key): \(value)")
             }
-        }
+            displayBarChart()
+       }
     }
     
     //MARK: - HealthKit Authorization Methods
@@ -158,9 +214,10 @@ class ViewController: UIViewController {
         if HKHealthStore .isHealthDataAvailable() {
             healthStore.requestAuthorizationToShareTypes(nil, readTypes: dataTypesToRead(), completion: { (success, error) in
                 if success {
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.getUserData()
-                    })
+                    print("Success")
+//                    dispatch_async(dispatch_get_main_queue(), {
+//                        self.getUserData()
+//                    })
                 } else {
                     print("Error: \(error)")
                 }
